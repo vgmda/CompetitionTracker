101390f0e1d4cddd507561c2a4f0864fe38d3162
vgmda <vasilegancin@gmail.com>
2022-05-05 22:48:38 +0100

    Add Prize model

---
+++ CompetitionLibrary/Prize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CompetitionLibrary
+{
+    public class Prize
+    {
+        public int PlaceNumber { get; set; }
+
+        public string PlaceName { get; set; }
+
+        public decimal PrizeAmount { get; set; }
+
+        public double PrizePercentage { get; set; }
+
+
+    }
+}
+
