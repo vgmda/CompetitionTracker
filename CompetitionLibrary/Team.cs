fafdfee7b29eaa39774401e20fd87bdb7941d43a
vgmda <vasilegancin@gmail.com>
2022-05-05 22:36:48 +0100

    Added Team model

---
+++ CompetitionLibrary/Team.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CompetitionLibrary
+{
+    public class Team
+    {
+        public List<Person> TeamMembers { get; set; } = new List<Person>();
+
+        public string TeamName { get; set; }
+
+    }
+}
+
