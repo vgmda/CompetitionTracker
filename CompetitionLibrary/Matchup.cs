4dcee4668a1b5c5acd1adfbf38ce0f99acfb99b2
vgmda <vasilegancin@gmail.com>
2022-05-05 22:52:10 +0100

    Add Matchup model

---
+++ CompetitionLibrary/Matchup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CompetitionLibrary
+{
+    public class Matchup
+    {
+        public List<MatchupEntry> Entries { get; set; } = new List<MatchupEntry>();
+
+        public Team Winner { get; set; }
+
+        public int MatchupRound { get; set; }
+
+
+    }
+}
+
