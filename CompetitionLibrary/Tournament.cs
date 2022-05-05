301b719df022fb310bca65918129a12405fbd01f
vgmda <vasilegancin@gmail.com>
2022-05-05 22:46:14 +0100

    Add Tournament model

---
+++ CompetitionLibrary/Tournament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CompetitionLibrary
+{
+    public class Tournament
+    {
+        public string TournamentName { get; set; }
+
+        public decimal EntryFee { get; set; }
+
+        public List<Team> EnteredTeams { get; set; } = new List<Team>();
+
+        public List<Prize> Prizes { get; set; } = new List<Prize>();
+
+        public List<List<Matchup>> Rounds { get; set; } = new List<List<Matchup>>();
+
+    }
+}
+
