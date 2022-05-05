d6d2a59b62bb011163271ab352cd5428fb1b441f
vgmda <vasilegancin@gmail.com>
2022-05-05 22:40:23 +0100

    Add Person model

---
+++ CompetitionLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CompetitionLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string MobileNumber { get; set; }
+
+    }
+}
+
