b47df888af33ea159a88289c4e07b98a14dd0951
vgmda <vasilegancin@gmail.com>
2022-05-05 23:23:08 +0100

    Comments

--- CompetitionLibrary/MatchupEntry.cs
+++ CompetitionLibrary/MatchupEntry.cs
@@ -3,10 +3,20 @@ namespace CompetitionLibrary
 {
     public class MatchupEntry
     {
+        /// <summary>
+        /// Reprsents one team in the matchup.
+        /// </summary>
         public Team TeamCompeting { get; set; }
 
+        /// <summary>
+        /// Represents the score for this particual team.
+        /// </summary>
         public double Score { get; set; }
 
+        /// <summary>
+        /// Represents the matchup that this team came
+        /// from as the winner.
+        /// </summary>
         public Matchup ParentMatchup { get; set; }
 
 
