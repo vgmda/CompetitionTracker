691133fc11b1477d86c99f7cfbbd4ee1dc0f141a
vgmda <vasilegancin@gmail.com>
2022-05-09 23:02:51 +0100

    New SQL Connection List

--- CompetitionLibrary/GlobalConfig.cs
+++ CompetitionLibrary/GlobalConfig.cs
@@ -8,13 +8,15 @@ namespace CompetitionLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();    
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if (database)
             {
-                // TODO - Create the SQL Connection
+                // TODO - Set up the SQL Connector properly
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
             }
 
             if (textFiles)
