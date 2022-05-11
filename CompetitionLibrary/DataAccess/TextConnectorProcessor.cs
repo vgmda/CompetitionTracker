d73955feba8602ac786bd8c349dcd3ee9b59be75
vgmda <vasilegancin@gmail.com>
2022-05-11 23:27:59 +0100

    Write LoadFile() method

--- CompetitionLibrary/DataAccess/TextConnectorProcessor.cs
+++ CompetitionLibrary/DataAccess/TextConnectorProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,21 @@ namespace CompetitionLibrary.DataAccess.TextConnector
 {
     public static class TextConnectorProcessor
     {
+        public static string FullFilePath(this string fileName) // fileName = Prize.csv
+        {
 
+            // C:\Users\vasile.gancin\Documents\CompetitionTracker\Prize.csv
+            return $"{ConfigurationManager.AppSettings["filePath"] }\\{ fileName } ";
+        }
 
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
     }
 }
